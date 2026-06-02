2ec76bf5f4730898251b706d680b748ec63a313c
lanstheprodigy <alan.040219.gam@gmail.com>
2026-06-02 17:22:31 +0700

    refactor: remove legacy menu redirect pages in favor of dashboard paths

--- src/app/menu/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function CreateMenuAliasPage() {
-  redirect("/dashboard/menu/create")
-}
--- src/app/menu/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function MenuAliasPage() {
-  redirect("/dashboard/menu")
-}
